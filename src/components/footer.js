5915d96a1b09634b777de70f3268e82d3696ec27
Aaron Shank <47933546+aaronshank@users.noreply.github.com>
2022-03-03 20:56:00 -0500

    footer styling

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,7 +6,7 @@ class Footer extends React.Component {
     return (
       <nav className='fixed-bottom'>
         <div>
-          <ul id='horizontal-list'>
+          <ul id='horizontal-list' className="footer">
             <li><a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>About</a></li>
             <li><a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>Safety Precautions</a></li>
             <li><a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ' target='_blank' rel='noreferrer'>Copyright</a></li>
